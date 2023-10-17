8acd5be2552fb06d64b01e4b25e1809f12504a5d
youngboiz1110 <139002385+youngboiz1110@users.noreply.github.com>
2023-10-17 17:02:22 +0700

    Add files via upload

---
+++ workshop3/src/Guitar.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Guitar {
+    private String serialNumber;
+    private int price;
+    private String builder;
+    private String model;
+    private String backWood;
+    private String topWood;
+
+    public Guitar() {
+    }
+
+    public Guitar(String serialNumber, int price, String builder, String model, String backWood, String topWood) {
+        this.serialNumber = serialNumber;
+        this.price = price;
+        this.builder = builder;
+        this.model = model;
+        this.backWood = backWood;
+        this.topWood = topWood;
+    }
+    
+    public void setSerialNumber(String serialNumber){
+        if(!serialNumber.isEmpty()){
+            this.serialNumber = serialNumber;
+        }
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getBuilder() {
+        return builder;
+    }
+
+    public void setBuilder(String builder) {
+        this.builder = builder;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBackWood() {
+        return backWood;
+    }
+
+    public void setBackWood(String backWood) {
+        this.backWood = backWood;
+    }
+
+    public String getTopWood() {
+        return topWood;
+    }
+
+    public void setTopWood(String topWood) {
+        this.topWood = topWood;
+    }
+    public void createSound(){
+        System.out.println("Serial Number: " + serialNumber);
+        System.out.println("Price: $" + price);
+        System.out.println("Builder: " + builder);
+        System.out.println("Model: " + model);
+        System.out.println("Back Wood: " + backWood);
+        System.out.println("Top Wood: " + topWood); 
+    }
+    
+}
