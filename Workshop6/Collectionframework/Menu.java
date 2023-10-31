89d5477e6624fdeed0b884206cf3211d754b492a
youngboiz1110 <139002385+youngboiz1110@users.noreply.github.com>
2023-10-31 16:09:57 +0700

    Add files via upload

---
+++ Workshop6/Collectionframework/Menu.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author My Computer
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Menu {
+    public static int getChoice(ArrayList options){
+        for (int i = 0; i < options.size(); i++) {
+            System.out.print((i+1) + "-" + options.get(i));;
+        }
+        System.out.print("Choose1..." + options.size() + ": ");
+        Scanner sc = new Scanner(System.in);
+        return Integer.parseInt(sc.nextLine());
+    }
+    public static int getChoice(Object[] options){
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i+1) + "-" + options[i]);
+        }
+        System.out.println("Choose1..." + options.length + ": ");
+        Scanner sc = new Scanner(System.in);
+        return Integer.parseInt(sc.nextLine());
+    }
+}
